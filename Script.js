27b1dd2c9f9988ff112e267a9c2bbd6e3123e280
Hari Krishna <hari.12029@gmail.com>
2022-12-12 09:18:02 +0530

    Complete all user registration

--- Script.js
+++ Script.js
@@ -1,7 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-
-    // alert('hi this is page load time')
-
     $.ajax({
         url: 'https://localhost:44382/api/Register/GetAllStates',
         method: 'get',
@@ -11,111 +8,128 @@ document.addEventListener("DOMContentLoaded", () => {
             $.each(data.data, function(key,val) {
                 $('#stateList').append( '<option value="'+val.id+'">'+val.stateName+'</option>' );
               });
-         alert("Saved successfully");
         },
        fail : function( jqXHR, textStatus ) {
          alert( "Request failed: " + textStatus );
        }
     })
-  });
+});
+toastr.options = {
+    positionClass: 'toast-top-center'
+};
 
+toastr.options.onHidden = function(){
+    window.location.reload();
+  }
 
-var selectedRow = null
+$(function() {
+    $("#stateList").change(function() {
+        let stateId = $('#stateList').val();
+     $.ajax({
+        url: 'https://localhost:44382/api/Register/GetAllCitiesByState?id='+stateId,
+        method: 'get',
+        dataType: 'json',
+        contentType: 'application/json; charset=utf-8',
+        success: function (data) {
+            $.each(data.data, function(key,val) {
+                $('#cityList').append( '<option value="'+val.id+'">'+val.cityName+'</option>' );
+              });
+        },
+       fail : function( jqXHR, textStatus ) {
+         alert( "Request failed: " + textStatus );
+       }
+    })
+    });
+});
+
+function GetRadioButtonSelection(){
+    var reasons= document.getElementsByName("gender");
+var answer;
+for (var j = 0; j < reasons.length; j++) {
+    if (reasons[j].checked) {
+        answer = reasons[j].value;
+        if(answer==='male')
+           answer = 1;
+        else
+           answer = 2;   
+    }
+}
+return answer;
+}
+
+function SubmitData() {
+        let registerData = {};
+        registerData.FirstName = document.getElementById('FirstName').value;
+        registerData.LastName = $('#LastName').val();
+        registerData.Mobile = document.getElementById('Mobile').value;
+        registerData.Dob = document.getElementById('DateOfBirth').value;
+        registerData.genderId = GetRadioButtonSelection();
+        registerData.stateId = $('#stateList').val();
+        registerData.cityId = $('#cityList').val();
+        registerData.email = document.getElementById('email').value;
+        registerData.Password = document.getElementById('Password').value;
+        registerData.conformPassword = document.getElementById('conformPassword').value;
+
+     $.ajax({
+        url: 'https://localhost:44382/api/Register/CreateRegister',
+        method: 'post',
+        dataType: 'json',
+        data: JSON.stringify(registerData),
+        contentType: 'application/json; charset=utf-8',
+        success: function (data) {
+            toastr.success('Registered Successfully',{
+                timeOut: 2000,
+                positionClass: 'toast-top-center',
+                // Redirect 
+                onHidden: function() {
+                    window.location.href = 'Login.html';
+                }});
+             window.location.href = 'Login.html';
+        },
+       fail : function( jqXHR, textStatus ) {
+         alert( "Request failed: " + textStatus );
+       }
+    })
+  
+};
 
 function onFormSubmit() {
-    if (validate()) {
-        // let formData = readFormData();
-        // if (selectedRow == null)
-        //   insertNewRecord(formData);
-        // else
-        //     updateRecord(formData);
-        // resetForm();
-        // if((document.forms.g[0].checked==false)  && ( document.form.g[1].checked==false))
-        // {
-        //     alert("please select your gender");
-        //     return false;
-        // }
+    if (validateRegisterForm()) {
+        SubmitData();
+    }
+    else 
+    {
+      toastr.error('Form is Filled with Invalid Data');
     }
 }
 
-// function readFormData() {
-//     let formData = {};
-//     // formData["FirstName"] = document.getElementById("FirstName").value;
-//     // formData["LastName"] = document.getElementById("LastName").value;
-//     // formData["email"] = document.getElementById("email").value;
-//     // formData["Password"] = document.getElementById("Password").value;
-//     // formData["Mobile"] = document.getElementById("Mobile").value;
-//     // formData["DateOfBirth"] = document.getElementById("DateOfBirth").value;
-//     // formData["gender"]=document.querySelector('input[name="gender"]:checked').value;
+function LoginSubmit() {
+    if (validateLoginForm()) {
+    let userName = $('#email').val();
+    let password = $('#Password').val();
+     $.ajax({
+        url: 'https://localhost:44382/api/Register/Login?userName='+userName+'&password='+password,
+        method: 'get',
+        dataType: 'json',
+        contentType: 'application/json; charset=utf-8',
+        success: function (data) {
+        toastr.success('Logged in Successfully');
+        if(data.data === true){
+            window.location.href = 'Home.html';
+        }
+        },
+       fail : function( jqXHR, textStatus ) {
+         alert( "Request failed: " + textStatus );
+       }
+    })
+}
+else{
+    toastr.error('Please Enter Correct Credentials');
 
-//     // return formData;
-// }
+}
+};
 
-// function insertNewRecord(data) {
-//     var table = document.getElementById("RegisterList").getElementsByTagName('tbody')[0];
-//     var newRow = table.insertRow(table.length);
-//     cell1 = newRow.insertCell(0);
-//     cell1.innerHTML = data.FirstName;
-//     cell2 = newRow.insertCell(1);
-//     cell2.innerHTML = data.LastName;
-//     cell3 = newRow.insertCell(2);
-//     cell3.innerHTML = data.email;
-//     cell4 = newRow.insertCell(3);
-//     cell4.innerHTML = data.Password;
-//     cell5 = newRow.insertCell(4);
-//     cell5.innerHTML = data.Mobile;
-//     cell6 = newRow.insertCell(5);
-//     cell6.innerHTML = data.DateOfBirth;
-//     cell7 = newRow.insertCell(6);
-//     cell7.innerHTML = data.gender;
-//     cell8 = newRow.insertCell(7);
-//     cell8.innerHTML = `<a onClick="onEdit(this)">Edit</a>
-//                        <a onClick="onDelete(this)">Delete</a>`;
-// }
-
-// function resetForm() {
-//     document.getElementById("FirstName").value = "";
-//     document.getElementById("LastName").value = "";
-//     document.getElementById("email").value = "";
-//     document.getElementById("Password").value = "";
-//     document.getElementById("Mobile").value = "";
-//     document.getElementById("DateOfBirth").value = "";
-//     document.getElementsByName("gender").value = "";
-    
-//     selectedRow = null;
-// }
-
-// function onEdit(td) {
-//     selectedRow = td.parentElement.parentElement;
-//     document.getElementById("FirstName").value = selectedRow.cells[0].innerHTML;
-//     document.getElementById("LastName").value = selectedRow.cells[1].innerHTML;
-//     document.getElementById("email").value = selectedRow.cells[2].innerHTML;
-//     document.getElementById("Password").value = selectedRow.cells[3].innerHTML;
-//     document.getElementById("Mobile").value = selectedRow.cells[3].innerHTML;
-//     document.getElementById("DateOfBirth").value = selectedRow.cells[3].innerHTML;
-//     document.getElementsByName("gender").value = selectedRow.cells[4].innerHTML;
-//     document.getElementById("btnSubmit").value = 'Update';
-// }
-// function updateRecord(formData) {
-//     selectedRow.cells[0].innerHTML = formData.FirstName;
-//     selectedRow.cells[1].innerHTML = formData.LastName;
-//     selectedRow.cells[2].innerHTML = formData.email;
-//     selectedRow.cells[3].innerHTML = formData.Password;
-//     selectedRow.cells[4].innerHTML = formData.Mobile;
-//     selectedRow.cells[4].innerHTML = formData.DateOfBirth;
-//     selectedRow.cells[4].innerHTML = formData.gender;
-//     document.getElementById("btnSubmit").value = 'Submit';
-
-// }
-
-// function onDelete(td) {
-//     if (confirm('Are you sure to delete this record ?')) {
-//         row = td.parentElement.parentElement;
-//         document.getElementById("RegisterList").deleteRow(row.rowIndex);
-//         resetForm();
-//     }
-// }
-function validate() {
+function validateRegisterForm() {
     let isValid = true;
     if (document.getElementById("FirstName").value == "") {
         isValid = false;
@@ -213,6 +227,32 @@ function validate() {
         if (!document.getElementById("CityValidationError").classList.contains("hide"))
             document.getElementById("CityValidationError").classList.add("hide");
     }
+    return isValid;
+    
+}
+
+function validateLoginForm() {
+    let isValid = true;
+
+    if (document.getElementById("email").value == "") {
+        isValid = false;
+        document.getElementById("emailValidationError").classList.remove("hide");
+    } 
+    else {
+        isValid = true;
+        if (!document.getElementById("emailValidationError").classList.contains("hide"))
+            document.getElementById("emailValidationError").classList.add("hide");
+    }
+    if (document.getElementById("Password").value == "") {
+        isValid = false;
+        document.getElementById("PasswordValidationError").classList.remove("hide");
+    } 
+    else {
+        isValid = true;
+        if (!document.getElementById("PasswordValidationError").classList.contains("hide"))
+            document.getElementById("PasswordValidationError").classList.add("hide");
+    }
+
     return isValid;
     
 }
